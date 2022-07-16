475c1114243c3dcc003ed40509ce0cd847886c01
rohit <rkg10072000@gmail.com>
2022-07-16 17:48:56 +0530

    injected user model in auth module and created basic sign-in and sign-up logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,37 @@
-import { Injectable } from "@nestjs/common"
+import { ConflictException, HttpException, Injectable, NotFoundException } from "@nestjs/common"
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
+import { User, UserDocument } from "src/user/schemas";
 import { AuthDto } from "./dtos"
 
 @Injectable()
 export class AuthService {
-  signup(dto: AuthDto) {
-    console.log(dto);
-    return {msg: "I am signed up"}
+
+  constructor(@InjectModel(User.name) private userModel : Model<UserDocument>){}
+
+  async signup(dto: AuthDto) {
+    try {
+      const user = await this.userModel.create(dto);
+      return user;
+    } catch (error) {
+      if (error.code === 11000) {
+        throw new ConflictException({
+          msg: `${dto.email} is already registered`,
+          status: "failed"
+        });
+      }
+      throw new HttpException(error.message, 400);
+    }
   }
-  signin(dto: AuthDto) {
-    console.log(dto);
-    return {msg: "I am signed in"}
+
+  async signin(dto: AuthDto) {
+    const user = await this.userModel.findOne({ email: dto.email });
+    if (!user) {
+      throw new NotFoundException({
+        msg: `user with email ${dto.email} not found`,
+        status: "failed"
+      })
+    }
+    return user;
   }
 }
